a7b122054dd0a125d13649cffbe63c7eaab75f6f
Daku0101 <dakuhacker01@gmail.com>
2024-10-01 18:49:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8107141003:AAFO2tC43UE_PLf4WBTcisSSVB9_nPjpjVA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@DiscoRoot")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1627436243")
+
+
+
+
+
+
+
+  
